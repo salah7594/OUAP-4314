cffb5731a23e72641a8953f27ea041d6c8598bf9
root <nicolasvo95@gmail.com>
2018-03-24 17:29:19 +0000

    added multithreading

--- Evaluation/Projet/flask_bdgest/run.py
+++ Evaluation/Projet/flask_bdgest/run.py
@@ -2,4 +2,4 @@ from flask import Flask
 from app import app
 
 if __name__ == '__main__':
-    app.run(debug=True, port=1000, host='0.0.0.0') 
+    app.run(debug=True, port=1000, host='0.0.0.0', threaded=True) 
